67669be6d398d492600362bf5970b427db97fd75
Alvaro Bartolome <alvaro@argilla.io>
2024-03-02 20:01:02 +0100

    Add `upload_directory_to_gcs`

---
+++ src/vertex_ai_huggingface_inference_toolkit/google_cloud/storage.py
@@ -0,0 +1,29 @@
+import os
+import warnings
+from pathlib import Path
+from typing import Optional
+
+from google.cloud import storage
+
+
+def upload_directory_to_gcs(
+    local_dir: str, bucket_name: Optional[str] = None, remote_dir: Optional[str] = None
+) -> str:
+    client = storage.Client()
+
+    bucket = client.bucket(bucket_name)
+    if not bucket.exists():
+        warnings.warn(
+            f"Bucket '{bucket_name}' does not exist. Creating it now.", stacklevel=1
+        )
+        client.create_bucket(bucket_name)
+
+    local_path = Path(local_dir)
+    for local_file in local_path.glob("**/*"):
+        if local_file.is_file():
+            relative_path = local_file.relative_to(local_path)
+            remote_file_path = os.path.join(remote_dir or "", str(relative_path))
+
+            blob = bucket.blob(remote_file_path)
+            blob.upload_from_filename(str(local_file))
+    return bucket.path
