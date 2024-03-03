89f997b9f2558da359a70af5768c93173fc1db57
Alvaro Bartolome <alvaro@argilla.io>
2024-03-03 15:14:11 +0100

    Update `TransformersModel` (WIP)

--- src/vertex_ai_huggingface_inference_toolkit/transformers.py
+++ src/vertex_ai_huggingface_inference_toolkit/transformers.py
@@ -1,7 +1,16 @@
 import warnings
 from typing import Dict, List, Literal, Optional
 
-from vertex_ai_huggingface_inference_toolkit.docker import build_docker_image
+from google.auth import default
+from google.cloud import aiplatform
+
+from vertex_ai_huggingface_inference_toolkit.docker import (
+    build_docker_image,
+    configure_docker_and_push_image,
+)
+from vertex_ai_huggingface_inference_toolkit.google_cloud.artifact_registry import (
+    create_repository_in_artifact_registry,
+)
 from vertex_ai_huggingface_inference_toolkit.google_cloud.storage import (
     upload_directory_to_gcs,
 )
@@ -18,6 +27,8 @@ class TransformersModel:
 
     def __init__(
         self,
+        project_id: Optional[str] = None,
+        location: Optional[str] = None,
         model_name_or_path: Optional[str] = None,
         model_bucket_uri: Optional[str] = None,
         framework: Literal["torch", "tensorflow", "flax"] = "torch",
@@ -39,6 +50,20 @@ class TransformersModel:
                 "You can't provide both `model_name_or_path` and `model_bucket_uri`"
             )
 
+        if project_id is None:
+            # https://google-auth.readthedocs.io/en/master/reference/google.auth.html
+            _, project_id = default()
+        self.project_id = project_id
+
+        if location is None:
+            warnings.warn(
+                "`location` has not been provided, so `location=us-central1` will be used"
+                " instead, as that's the Google Cloud default region.",
+                stacklevel=1,
+            )
+            location = "us-central1"
+        self.location = location
+
         if model_bucket_uri is not None:
             self.model_bucket_uri = model_bucket_uri
 
@@ -46,17 +71,34 @@ class TransformersModel:
             _local_dir = download_files_from_hub(
                 repo_id=model_name_or_path, framework=framework
             )
-            self.model_bucket_uri = upload_directory_to_gcs(local_dir=_local_dir)
+            self.model_bucket_uri = upload_directory_to_gcs(
+                project_id=self.project_id,
+                location=self.location,
+                local_dir=_local_dir,  # type: ignore
+            )
 
-        # TODO: Push the local Docker image to the Artifact Registry
-        self.image = custom_image_uri or build_docker_image(
-            python_version=python_version or "3.10",
-            framework=framework or "torch",
-            framework_version=framework_version or "2.1.0",
-            transformers_version=transformers_version or "4.38.2",
-            cuda_version=cuda_version,
-            extra_requirements=extra_requirements,
-        )
+        if custom_image_uri is None:
+            _image = build_docker_image(
+                python_version=python_version or "3.10",
+                framework=framework or "torch",
+                framework_version=framework_version or "2.1.0",
+                transformers_version=transformers_version or "4.38.2",
+                cuda_version=cuda_version,
+                extra_requirements=extra_requirements,
+            )
+            create_repository_in_artifact_registry(
+                project_id=self.project_id,  # type: ignore
+                location=self.location,
+                name="vertex-ai-huggingface-inference-toolkit",
+                format="DOCKER",
+            )
+            custom_image_uri = configure_docker_and_push_image(
+                project_id=self.project_id,  # type: ignore
+                location=self.location,
+                repository="vertex-ai-huggingface-inference-toolkit",
+                image_with_tag=_image,
+            )
+        self.image_uri = custom_image_uri
 
         if environment_variables is None:
             environment_variables = {}
@@ -67,16 +109,20 @@ class TransformersModel:
             )
             environment_variables["VERTEX_CPR_WEB_CONCURRENCY"] = "1"
 
+        display_name = (
+            model_name_or_path.replace("/", "--")
+            if model_name_or_path is not None
+            else model_bucket_uri.split("/")[-1]  # type: ignore
+        )
+
+        aiplatform.init(project=self.project_id, location=self.location)
         # https://github.com/googleapis/python-aiplatform/blob/63ad1bf9e365d2f10b91e2fd036e3b7d937336c0/google/cloud/aiplatform/models.py#L2974
-        # from google.cloud import aiplatform
-        # aiplatform.init(project=project_id, location=region)
-        # self._model: aiplatform.Model = aiplatform.Model.upload(
-        #     display_name=model_name_or_path.replace("/", "--"),  # type: ignore
-        #     artifact_uri="gs://huggingface-cloud/bart-large-mnli",  # Should be created if not provided
-        #     serving_container_image_uri="",  # framework, framework_version, cpu/gpu, cuda_version
-        #     # if extra requirements then build an image installing those libraries on top of serving_container_image_uri
-        #     serving_container_environment_variables=environment_variables,
-        # )
+        self._model: aiplatform.Model = aiplatform.Model.upload(
+            display_name=display_name,
+            artifact_uri=self.model_bucket_uri,
+            serving_container_image_uri=self.image_uri,
+            serving_container_environment_variables=environment_variables,
+        )
 
     @classmethod
     def from_bucket(cls, bucket_name: str) -> "TransformersModel":  # type: ignore
