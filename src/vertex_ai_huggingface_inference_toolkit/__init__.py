49d6e48fa7cb47c68f2b8d24f4d84f064ec32b70
Alvaro Bartolome <alvaro@argilla.io>
2024-03-04 21:12:43 +0100

    Set `0.0.1.b10` version

--- src/vertex_ai_huggingface_inference_toolkit/__init__.py
+++ src/vertex_ai_huggingface_inference_toolkit/__init__.py
@@ -1,7 +1,7 @@
 """`vertex_ai_huggingface_inference_toolkit `: 🤗 Hugging Face Inference Toolkit for Google Cloud Vertex AI (similar to SageMaker's Inference Toolkit, but unofficial)"""
 
 __author__ = "Alvaro Bartolome <alvarobartt@gmail.com>"
-__version__ = "0.0.1.b9"
+__version__ = "0.0.1.b10"
 
 from vertex_ai_huggingface_inference_toolkit.transformers import TransformersModel
 
