73baffedaec709d506cf2ac69aa598bec8f1f95b
Alvaro Bartolome <alvaro@argilla.io>
2024-03-07 15:32:56 +0100

    Add remaining docstrings and code-comments

--- src/vertex_ai_huggingface_inference_toolkit/google_cloud/artifact_registry.py
+++ src/vertex_ai_huggingface_inference_toolkit/google_cloud/artifact_registry.py
@@ -12,23 +12,46 @@ from google.cloud.artifactregistry_v1beta2.types.repository import Repository
 def create_repository_in_artifact_registry(
     project_id: str, location: str, name: str, format: str = "DOCKER"
 ) -> str:
+    """Creates a new repository in Google Cloud Artifact Registry.
+
+    Args:
+        project_id: is either the name or the identifier of the project in Google Cloud.
+        location: is the identifier of the region and zone where the repository will be created.
+        name: is the name of the repository to be created.
+        format: is the format of the repository to be created, which by default is `DOCKER`.
+
+    Returns:
+        The path to the repository that has been created in Google Artifact Registry.
+
+    Raises:
+        RuntimeError: if the repository already exists but its `format` doesn't match
+            the specifiec via `format` arg.
+    """
+
     client = ArtifactRegistryClient()
     parent = f"projects/{project_id}/locations/{location}"
 
+    # Creates the request payload to create a repository in Google Artifact Registry
+    # with the specified `name` and `format`, under the specified `project_id` and `location`
     request = CreateRepositoryRequest(
         parent=parent,
         repository_id=name,
         repository=Repository(format_=format),
     )
     try:
+        # Sends the request to create the repository in Google Artifact Registry
         response = client.create_repository(request=request)
         return response.result().name  # type: ignore
     except exceptions.AlreadyExists as ae:
         repository_path = f"{parent}/repositories/{name}"
 
+        # Sends a request to get the information of the existing repository, if it exists
+        # then checks if it's a repository matching `format`, if not, raises an exception.
         request = GetRepositoryRequest(name=repository_path)
-
         response = client.get_repository(request=request)
+
+        # Checks the format of the existing repository, if it's not a `format` repository
+        # then raises an exception.
         response_format = Repository.Format(response.format_).name
         if response_format != format:
             raise RuntimeError(
@@ -41,7 +64,7 @@ def create_repository_in_artifact_registry(
             f"Skipping `repository={name}` creation since it already exists!",
             stacklevel=1,
         )
-        return f"{parent}/repositories/{name}"
+        return repository_path
     except Exception as e:
         raise RuntimeError(
             f"`repository={name}` couldn't be created with exception `{e}`"
--- src/vertex_ai_huggingface_inference_toolkit/predictors/transformers.py
+++ src/vertex_ai_huggingface_inference_toolkit/predictors/transformers.py
@@ -12,11 +12,40 @@ from vertex_ai_huggingface_inference_toolkit.utils import get_device
 
 
 class TransformersPredictor(Predictor):
+    """Custom `Predictor` for the Hugging Face `transformers` library, that allows
+    to load the model and run inference on top of it via the `pipeline` method. This
+    class is also in charge of downloading the artifacts from Google Cloud Storage, when
+    provided, and loading the model with a custom configuration and with automatic device
+    placement, mostly via `accelerate`.
+    """
+
     def __init__(self) -> None:
+        """Initializes the `TransformersPredictor` with a custom logger."""
         self._logger = get_logger("vertex-ai-huggingface-inference-toolkit")
 
     def load(self, artifacts_uri: Optional[str] = None) -> None:
-        """Loads the preprocessor and model artifacts."""
+        """Downloads the model from the given `artifacts_uri` or `HF_HUB_ID` environment
+        variable, to load it via `pipeline` and placing it on the right device. So on, the
+        outcome of `load` is the assignment of the value for the `_pipeline` attribute, that
+        will be used to run the inference via `predict`.
+
+        The `load` method is called within the CPR server during the initialization of the
+        server, so it's the first method to be called before running the inference.
+
+        Args:
+            artifacts_uri: is the Google Cloud Storage URI to the artifact to serve, which
+                will ideally be the directory where the model is stored in Google Cloud Storage.
+                Also note it's optional not because the model can be loaded via `HF_HUB_ID`, because
+                its mandatory to provide a Google Cloud Storage URI in Vertex AI, but because it
+                can be used locally without the need of an artifact URI, as the model can be pulled
+                from the Hugging Face Hub.
+
+        Raises:
+            RuntimeError: if neither the `artifacts_uri` nor the `HF_HUB_ID` environment variable
+                are set, as the model needs to be loaded from somewhere.
+        """
+
+        # If the `artifacts_uri` is provided, then we download its contents into the current directory
         if artifacts_uri is not None:
             self._logger.info(
                 f"Downloading artifacts from `artifacts_uri='{artifacts_uri}'`"
@@ -24,6 +53,9 @@ class TransformersPredictor(Predictor):
             prediction_utils.download_model_artifacts(artifacts_uri)
             self._logger.info("Artifacts successfully downloaded!")
 
+        # If the `artifacts_uri` was provided, but the `model.tar.gz` file was not downloaded from it,
+        # and the `HF_HUB_ID` environment variable is not set, then we raise an error as the model needs
+        # to be loaded from somewhere.
         hub_id = os.getenv("HF_HUB_ID", None)
         file_exists = os.path.exists("model.tar.gz") and os.path.isfile("model.tar.gz")
         if not file_exists and hub_id is None:
@@ -31,6 +63,8 @@ class TransformersPredictor(Predictor):
             self._logger.error(error_msg)
             raise RuntimeError(error_msg)
 
+        # If the `artifacts_uri` was provided, and the `model.tar.gz` file was downloaded from it, then
+        # we set the `model_path` to the `transformers-model` directory, otherwise we set it to the `HF_HUB_ID`.
         model_path = "./transformers-model" if file_exists else hub_id
         if file_exists:
             if hub_id:
@@ -39,16 +73,20 @@ class TransformersPredictor(Predictor):
                     f" variable `HF_HUB_ID={hub_id}` are set, the `artifacts_uri` will be used as"
                     " it has priority over the `HF_HUB_ID` environment variable."
                 )
+            # Extract the `model.tar.gz` file into the `transformers-model` directory
             os.makedirs("./transformers-model", exist_ok=True)
             with tarfile.open("model.tar.gz", "r:gz") as tar:
                 tar.extractall(path="./transformers-model")
 
+        # If the `HF_MODEL_KWARGS` environment variable is set, then we parse its value into a dictionary
         model_kwargs = os.getenv("HF_MODEL_KWARGS", None)
         model_kwargs_dict: Dict[str, Any] = {}
         if model_kwargs is not None:
             try:
                 model_kwargs_dict = eval(model_kwargs)
                 self._logger.info(f"HF_MODEL_KWARGS value is {model_kwargs_dict}")
+                # Since the device placement is in charge of the `TransformersPredictor`, we will pop those
+                # keys from the `model_kwargs_dict` to avoid conflicts with the `pipeline` method.
                 model_kwargs_dict.pop("device", None)
                 model_kwargs_dict.pop("device_map", None)
             except Exception:
@@ -56,11 +94,17 @@ class TransformersPredictor(Predictor):
                     f"Failed to parse `HF_MODEL_KWARGS` environment variable: {model_kwargs}"
                 )
 
+        # If the `HF_TASK` environment variable is set, then we use it to load the `pipeline` with the
+        # specified task, otherwise we load the `pipeline` with the default task, which is inferred from
+        # the model's architecture.
         task = os.getenv("HF_TASK", "")
         if task != "":
             self._logger.info(f"HF_TASK value is {task}")
 
         try:
+            # First we try to load the `pipeline` using `device_map='auto'` from `accelerate`, but since some
+            # architectures do not support it, we will catch the exception and try to load the `pipeline` using
+            # the `device` instead.
             self._logger.info("Loading `pipeline` using `device_map='auto'`")
             self._pipeline = pipeline(
                 task,
@@ -72,11 +116,15 @@ class TransformersPredictor(Predictor):
             self._logger.error(
                 f"Failed to load `pipeline` using `device_map='auto'` failed with exception: {ve}"
             )
-            # Some models like `DebertaV2ForSequenceClassification` do not support `device_map='auto'`
+
+            # If the exception was not raised because the model does not support `device_map='auto'`, then we
+            # re-raise the exception, otherwise we load the `pipeline` using the `device` instead.
             pattern = re.compile(r"[a-zA-Z0-9]+ does not support `device_map='auto'`")
             if not pattern.search(str(ve)):
                 raise ve
 
+            # Some models like `DebertaV2ForSequenceClassification` do not support `device_map='auto'`, so the
+            # standard device placement via `device` is being used.
             device = get_device()
             self._logger.info(f"Loading `pipeline` using `device='{device}'` instead!")
             self._pipeline = pipeline(
@@ -91,8 +139,27 @@ class TransformersPredictor(Predictor):
         )
 
     def predict(self, instances: Dict[str, Any]) -> Dict[str, Any]:
+        """Runs the inference on top of the loaded `pipeline` with the given `instances`.
+
+        Args:
+            instances: is the dictionary containing the instances to be predicted, which can either
+                be a dictionary with `instances` as the key and the value being a list of dicts, or
+                directly a single instance with the expected keys by `pipeline`.
+
+        Returns:
+            The dictionary containing the predictions for the given `instances`.
+        """
+
+        # NOTE: the standard `predict` method assumes that the `instances` is a dictionary with the key
+        # `instances` that contains the actual instances to be predicted, so we need to check whether
+        # the `instances` is a dictionary or a list, and if it's a dictionary, then we need to extract
+        # the `instances` from it.
+        if "instances" in instances:
+            instances = instances["instances"]
+
         # NOTE: temporary patch for `text-classification` until the following PR is merged (if so):
         # https://github.com/huggingface/transformers/pull/29495
         if "args" in instances:
             return self._pipeline(instances.pop("args"), **instances)  # type: ignore
+
         return self._pipeline(**instances)  # type: ignore
